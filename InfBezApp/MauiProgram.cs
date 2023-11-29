7757cab77e2615bb6c63ecd7d672946712969f20
mlexanders <mlexanders@outlook.com>
2023-11-29 15:57:09 +0300

    ~

--- InfBezApp/MauiProgram.cs
+++ InfBezApp/MauiProgram.cs
@@ -14,7 +14,7 @@ namespace InfBezApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
-
+            
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
